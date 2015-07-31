5574ac619a67f77b275ad40f6af7ee55e8ed9989
Terry <tchan247@gmail.com>
2015-07-31 13:43:08 -0700

    (feat) refactor website file structure

--- client/app.js
+++ client/app.js
@@ -4,8 +4,17 @@ angular.module('app', ['ngRoute', 'ngResource', 'home-service', 'home-controller
 
   .config(function($routeProvider){
     $routeProvider
+    // .when('/login', {
+    //   templateUrl: 'admin/components/login/loginView.html',
+    //   controller: 'loginController'
+    // })
+    // .when('/edit', {
+    //   templateUrl: 'admin/components/edit/editView.html',
+    //   controller: 'editController'
+    // })
+
     .when('/blog', {
-      templateUrl: 'app/components/blog/blogView.html',
+      templateUrl: './client/website/components/blog/blogView.html',
       controller: 'blogController',
       resolve: {
         postsData: function(PostsService){
