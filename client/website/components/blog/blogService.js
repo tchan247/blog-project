80de6d1d2c3f7b409fd54e87b83d5aca6666162a
Terry <tchan247@gmail.com>
2015-08-06 23:42:35 -0700

    (feat) testing api key hiding feature

--- client/website/components/blog/blogService.js
+++ client/website/components/blog/blogService.js
@@ -3,19 +3,23 @@ angular.module('blog-service', [])
 
 .factory('PostsService', function($http){
 
-  getPosts = function() {
+  getHeaders = function() {
+    $http.get('config.json');
+  };
+
+  getPosts = function(headers) {
     return $http({
       method: 'GET', 
       url: 'https://api.parse.com/1/classes/blog_post', 
-      headers: {'X-Parse-Application-Id':'fCTyXGLEO67M6Ouf4bw4wgnsYxoE7vOvtgxkgzSj', 'X-Parse-REST-API-Key':'lj4Pqa5VjeugGRx4S2jM1YteZR1PUjt8iRm65LHU'}
+      headers: headers
     });
   };
 
-  getImages = function() {
+  getImages = function(headers) {
     return $http({
       method: 'GET', 
       url: 'https://api.parse.com/1/classes/blog_images', 
-      headers: {'X-Parse-Application-Id':'fCTyXGLEO67M6Ouf4bw4wgnsYxoE7vOvtgxkgzSj', 'X-Parse-REST-API-Key':'lj4Pqa5VjeugGRx4S2jM1YteZR1PUjt8iRm65LHU'}
+      headers: headers
     });
   };
 
