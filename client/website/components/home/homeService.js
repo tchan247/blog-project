fb716fd50a9f3926380b39ad5d62ec514ffe26e1
Terry <tchan247@gmail.com>
2016-08-18 23:38:44 -0700

    (clean) disable home service; move cunder-construction text;

--- client/website/components/home/homeService.js
+++ client/website/components/home/homeService.js
@@ -2,52 +2,52 @@ angular.module('home-service', [])
 
 .factory('homeService', function($http, $rootScope){
 
-  getHeaders = function() {
-    return $http.get('config.json');
-  };
+  // getHeaders = function() {
+  //   return $http.get('config.json');
+  // };
 
-  var getName = function(){
-    var nameRequest = function(headers) {
-      return $http({
-        method: 'GET', 
-        url: '', 
-        headers: headers
-      });
-    };
+  // var getName = function(){
+  //   var nameRequest = function(headers) {
+  //     return $http({
+  //       method: 'GET', 
+  //       url: '', 
+  //       headers: headers
+  //     });
+  //   };
 
-    if($rootScope.headers) {
-      return nameRequest($rootScope.headers);
-    } else {
-      return getHeaders()
-      .then(function(data){
-        return nameRequest(data.data);
-      });
-    }
+  //   if($rootScope.headers) {
+  //     return nameRequest($rootScope.headers);
+  //   } else {
+  //     return getHeaders()
+  //     .then(function(data){
+  //       return nameRequest(data.data);
+  //     });
+  //   }
 
-  };
+  // };
 
-  var getAboutMe = function(){
-    var aboutRequest = function(headers) {
-      return $http({
-        method: 'GET', 
-        url: 'https://api.parse.com/1/classes/about_me', 
-        headers: headers
-      });
+  // var getAboutMe = function(){
+  //   var aboutRequest = function(headers) {
+  //     return $http({
+  //       method: 'GET', 
+  //       url: 'https://api.parse.com/1/classes/about_me', 
+  //       headers: headers
+  //     });
 
-    };
+  //   };
 
-    if($rootScope.headers) {
-      return aboutRequest($rootScope.headers);
-    } else {
-      return getHeaders()
-      .then(function(data){
-        return aboutRequest(data.data);
-      });
-    }
-  };
+  //   if($rootScope.headers) {
+  //     return aboutRequest($rootScope.headers);
+  //   } else {
+  //     return getHeaders()
+  //     .then(function(data){
+  //       return aboutRequest(data.data);
+  //     });
+  //   }
+  // };
 
-  return {
-    getName: getName,
-    getAboutMe: getAboutMe
-  };
+  // return {
+  //   getName: getName,
+  //   getAboutMe: getAboutMe
+  // };
 });
